6eff58670a0a3f09f096450dc6866a32c1841a9a
MT <vominhthang061003@gmail.com>
2022-04-06 08:36:56 +0700

    gs

---
+++ site1/app.js
@@ -0,0 +1,34 @@
+(function () {
+'use strict';
+
+angular.module("LunchCheck", [])
+.controller("LunchCheckController", LController);
+LController.$inject= ['$scope'];
+
+    function LController($scope) {
+        $scope.text= "";
+        $scope.message= "Please enter data, I'll not count blank spaces"
+
+        $scope.checkAmount= function () {
+            var itemArray= $scope.text.split(","); 
+            var i, count= 0;
+           for (i= 0; i< itemArray.length; i++) {
+               if (itemArray[i]!= "" && itemArray[i]!= " ") count++;
+           }
+           if (count== 0) {
+               $scope.message= "Please enter data first";
+               $scope.textColor= "red";
+            }
+           else if (count<= 3) {
+               $scope.message= "Enjoy!";
+               $scope.textColor= "green";
+            }
+           else {
+               $scope.message= "Too much!";
+               $scope.textColor= "green";
+           }
+            
+           console.log(count);
+        }
+    }
+})();
